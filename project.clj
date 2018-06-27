bfb131259b8220a77c0450510ded3ca0cf569209
Wilker Lucio <wilkerlucio@gmail.com>
2018-06-27 01:44:30 -0300

    Add string gen

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject com.wsscode.test-chuck-string-from-regex-cljs "0.1.0-SNAPSHOT"
+(defproject com.wsscode.test-chuck-string-from-regex-cljs "0.2.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-beta1" :scope "provided"]
-                 [org.clojure/clojurescript "1.9.908" :scope "provided"]
-                 [org.clojure/test.check "0.10.0-alpha2" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
+                 [org.clojure/clojurescript "1.10.238" :scope "provided"]
+                 [org.clojure/test.check "0.10.0-alpha3" :scope "provided"]
                  [com.gfredericks/test.chuck "0.2.8" :scope "provided"]
                  [instaparse "1.4.8"]])
